cb0a1897bae5689cca5cd0be7a7c261f059dfdf1
Jonathan Elazar <jonathanelazar89@gmail.com>
2026-06-28 18:10:07 -0500

    Initial Commit

---
+++ scripts/move-P1.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+const SPEED = 300
+const JUMP_VELOCITY = -600.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	var direction := Input.get_axis("p1-left", "p1-right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if Input.is_action_just_pressed("p1-jump"):
+		velocity.y = JUMP_VELOCITY
+
+	
+	
+	move_and_slide()
